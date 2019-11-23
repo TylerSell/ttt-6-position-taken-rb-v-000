570105101cc2d58af658fd17d66a2f25acd3c152
Tyler Sell <tylerselldesign@gmail.com>
2019-11-23 20:22:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def position_taken?(board, index) # define position_taken? method 
   if board[index] == " " # begin if statement
     false
@@ -9,3 +10,14 @@ def position_taken?(board, index) # define position_taken? method
     true
   end # end if statement
 end # end of position_taken? method
+=======
+def position_taken?(board, index)
+  if board[index] == " " || ""
+    false
+  elsif board[index] == "X"
+    true
+  elsif board[index] == "O"
+    true
+  end
+end
+>>>>>>> 6da6413a30806a8ee652658116bf53d302c33d16
