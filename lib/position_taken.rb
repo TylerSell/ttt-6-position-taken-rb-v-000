95fbd5ec7816b58375f6304dc093e194cb42e604
Tyler Sell <tylerselldesign@gmail.com>
2019-11-23 20:27:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 def position_taken?(board, index)
   if board[index] == "X"
     true 
-  elsif 
+  elsif board[index] == "O"
     true
-  elsif board[index] == "O"board[index] == " " || ""
-    true
+  elsif board[index] == " " || ""
+    false
   end
 end
 
